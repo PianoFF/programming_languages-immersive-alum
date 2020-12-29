c1a5076453420afdb34009a832fb6d9a76be4e72
pianoFF <fangfei1star@gmail.com>
2020-12-29 17:47:39 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,49 @@
+require 'pry'
 def reformat_languages(languages)
   # your code here
+  result = {}
+  languages.each_pair do |k,v|
+    v.each_pair do |key,value|
+      if result[key]
+        result[key][:style].push(k)
+      else
+        result[key] = value
+        result[key][:style] = [k]
+      end
+    end
+  end
+  result 
 end
+
+
+# languages = {
+#   :oo => {
+#     :ruby => {
+#       :type => "interpreted"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     },
+#     :python => {
+#       :type => "interpreted"
+#     },
+#     :java => {
+#       :type => "compiled"
+#     }
+#   },
+#   :functional => {
+#     :clojure => {
+#       :type => "compiled"
+#     },
+#     :erlang => {
+#       :type => "compiled"
+#     },
+#     :scala => {
+#       :type => "compiled"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     }
+ 
+#   }
+# }
